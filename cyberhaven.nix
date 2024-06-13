538af8a2076ec0574e7b188002f9b6246cfdac44
Fede Barcelona <fede_rico_94@hotmail.com>
2024-06-13 16:58:32 +0200

    Initial commit

---
+++ cyberhaven.nix
@@ -0,0 +1,75 @@
+{
+  stdenv,
+  requireFile,
+  autoPatchelfHook,
+  makeWrapper,
+  dpkg,
+  openssl,
+  libgcc,
+  xz,
+  zlib,
+  bzip2,
+  keyutils,
+  buildFHSUserEnv,
+  writeShellScript,
+}: let
+  cyberhaven = stdenv.mkDerivation rec {
+    pname = "cyberhaven";
+    version = "24.04.01.56";
+    ref = "6cedb0";
+
+    src = requireFile {
+      name = "Cyberhaven-${version}-${ref}.deb";
+      url = "https://drive.google.com/drive/folders/12cIRewEoypqr0f5jmAXMW_iXZumTLf9h";
+      sha256 = "0qp5xzvds330sqvg4c08ny4fh23j0k1rr9jpws6xjijxdc4v50nh";
+    };
+
+    nativeBuildInputs = [
+      autoPatchelfHook
+      makeWrapper
+      dpkg
+    ];
+
+    buildInputs = [
+      openssl
+      libgcc.lib
+      xz
+      zlib
+      bzip2
+      keyutils.lib
+    ];
+
+    installPhase = ''
+      runHook preInstall
+
+      dpkg -X $src $out
+      rm $out/opt/cyberhaven/lib/libcyberhavennet-legacy.so
+
+      runHook postInstall
+    '';
+  };
+in
+  buildFHSUserEnv {
+    name = cyberhaven.pname;
+
+    targetPkgs = pkgs: [
+      cyberhaven
+      pkgs.openssl
+    ];
+
+    runScript = writeShellScript "cyberhaven" ''
+      backend="$1"
+      installToken="$2"
+      shift 2
+
+      mkdir -p /etc/opt/cyberhaven /var/lib/cyberhaven
+      mount -t tmpfs none /etc/opt/cyberhaven
+      mount -t tmpfs none /var/lib/cyberhaven
+
+      echo ">>> Installing cyberhaven"
+      ${cyberhaven}/opt/cyberhaven/cyberhaven --set-backend-url "$backend" --set-install-token "$installToken"
+
+      echo ">>> Running cyberhaven"
+      exec ${cyberhaven}/opt/cyberhaven/cyberhaven "$@"
+    '';
+  }
---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    cyberhaven = pkgs.callPackage ./cyberhaven.nix {};
+  in {
+    packages.${system} = rec {
+      inherit cyberhaven;
+      default = cyberhaven;
+    };
+
+    nixosModules.cyberhaven = import ./cyberhaven-module.nix {inherit cyberhaven;};
+
+    formatter.${system} = pkgs.alejandra;
+  };
+}
